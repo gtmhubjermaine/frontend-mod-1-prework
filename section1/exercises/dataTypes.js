acbca2b0842bf7c39c578307a86a84139e56ba6f
Jermaine Braumuller <jermaine@gtmhub.com>
2022-02-10 12:58:44 -0400

    variables

--- section1/exercises/dataTypes.js
+++ section1/exercises/dataTypes.js
@@ -9,53 +9,46 @@ When navigated to the root of this project directory, you should be able to run 
 file from your terminal with the command `node section1/exercises/dataTypes.js`
 */
 
-
 //-------------------
 //STRINGS
 //-------------------
 
 // EXAMPLE: Write code below to log the String "Alan Turing" in the console:
-console.log("Alan Turing");
+console.log("Alan Turing")
 
 // EXAMPLE: Write code below to log `Welcome to Turing!` in the console:
-console.log('Welcome to Turing!');
+console.log("Welcome to Turing!")
 
 // YOU DO: Write code below to log `99 bottles of pop on the wall...`:
-
+console.log("99 bottles of pop on the wall...")
 
 // YOU DO: Write code below to log one line from your favorite song or movie:
-
-
-
+console.log("where we're going, we don't need any roads...")
 //-------------------
 //NUMBERS
 //-------------------
 
 // EXAMPLE: log the result of the sum of 2 and 2 to the console:
-console.log(2 + 2);
+console.log(2 + 2)
 
 // EXAMPLE: log the result of 7 subtracted from 83 to the console:
-console.log(83 - 7);
+console.log(83 - 7)
 
 // YOU DO: log the result of 6 multiplied by 53 to the console:
-
-
+console.log(6 * 53)
 // YOU DO: log the result of 20 divided by 4 to console:
 
-
 // YOU DO: log the result of the modulo of 10 into 54:
-
-
-
+console.log()
 //-------------------
 // BOOLEANS
 //-------------------
 
 // EXAMPLE: log to the console the result of 1 is equal to 2:
-console.log(1 === 2);
+console.log(1 === 2)
 
 // EXAMPLE: log to the console the result of 7 is greater than 2:
-console.log(7 > 2);
+console.log(7 > 2)
 
 // YOU DO: log to the console the result of "hello" is equal to "Hello":
 
@@ -63,11 +56,9 @@ console.log(7 > 2);
 
 // YOU DO: log to the console the result of 4 is less than or equal to 5:
 
-
-
 //-------------------
 // FINAL CHECK
 //-------------------
 
-// Did you run this file in your terminal to make sure everything printed out to the console 
-// as you would expect? 
+// Did you run this file in your terminal to make sure everything printed out to the console
+// as you would expect?
--- section1/exercises/variables.js
+++ section1/exercises/variables.js
@@ -9,47 +9,52 @@ When navigated to the root of this project directory, you should be able to run 
 file from your terminal with the command `node section1/exercises/variables.js`
 */
 
-
 // EXAMPLE: Write code below to assign Harry Potter's name to a variable and
 // log that variable to the console:
-var name = "Harry Potter";
-console.log(name);
+var name = "Harry Potter"
+console.log(name)
 
 // EXAMPLE: Write code below to add 2 to the variable `students` and
 // log the result:
-var enrolledStudents = 22 + 2;
-console.log(enrolledStudents);
+var enrolledStudents = 22 + 2
+console.log(enrolledStudents)
 
 // YOU DO:
 // Write code below to save the string 'Harry Potter must not return to Hogwarts!'
 // log that variable to the console.
-
+var phrase = "Harry Potter must not return to Hogwarts!"
+console.log(phrase)
 
 // YOU DO:
-// Declare three variables, named `firstName`, `isHungry` and `numberOfPets`. 
-  // Store the appropriate data types in each.
+// Declare three variables, named `firstName`, `isHungry` and `numberOfPets`.
+// Store the appropriate data types in each.
 // log all three variables to the console.
+var firstName = "Jermaine"
+var isHungry = true
+var numberOfPets = 10
+console.log(firstName, isHungry, numberOfPets)
 
 // IN WORDS:
-// How did you decide to use the data type you did for each of the three variables above? 
-  // Explain.
-
+// How did you decide to use the data type you did for each of the three variables above?
+// Explain.
 
 // YOU DO:
-// Re-assign the values to the three variables from the previous challenge to different 
-  // values (but same data type).
+// Re-assign the values to the three variables from the previous challenge to different
+// values (but same data type).
 // log all three variables to the console.
-
+firstName = "Ana"
+isHungry = false
+numberOfPets = 6
+console.log(firstName, isHungry, numberOfPets)
 
 // YOU DO:
 // Using the variables below, log the total number of snacks to the console:
-var healthySnacks = 6;
-var junkFoodSnacks = 8;
-
-
+var healthySnacks = 6
+var junkFoodSnacks = 8
+console.log(healthySnacks + junkFoodSnacks)
 //-------------------
 // FINAL CHECK
 //-------------------
 
-// Did you run this file in your terminal to make sure everything printed out to the console 
-// as you would expect? 
+// Did you run this file in your terminal to make sure everything printed out to the console
+// as you would expect?
