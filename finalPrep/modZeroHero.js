dd62d3eac3bee0379aac137974639d5d18a990dd
Jermaine Braumuller <jermaine@gtmhub.com>
2022-05-12 11:45:32 +0200

    final prep

--- finalPrep/modZeroHero.js
+++ finalPrep/modZeroHero.js
@@ -1,49 +1,67 @@
 // Challenge - See if you can follow the instructions and complete the exercise in under 30 minutes!
 
 // Declare two variables - heroName AND specialAbility - set to strings
+var heroName = "Spiderman"
+var specialAbility = "Spidey Sense"
 
 // Declare two variables - greeting AND catchphrase
 //   greeting should be assigned to a string that uses concatenation to include the heroName
 //   catchphrase should be assigned to a string that uses interpolation to include the specialAbility
+var greeting = "Hello, My name is " + heroName
+var catchphrase = "My " + specialAbility + " is tingling"
 
 // Declare two variables - power AND energy - set to integers
-
+var power = 50
+var energy = 75
 // Declare two variables - fullPower AND fullEnergy
 //   fullPower should multiply your current power by 500
 //   fullEnergy should add 150 to your current energy
-
+var fullPower = power * 500
+var fullEnergy = (energy += 150)
 // Declare two variables - isHuman and identityConcealed - assigned to booleans
-
-
+var isHuman = true
+var identityConcealed = true
 // Declare two variables - archEnemies AND sidekicks
 //   archEnemies should be an array of at least 3 different enemy strings
 //   sidekicks should be an array of at least 3 different sidekick strings
-
+var archEnemies = ["Green Goblin", "Doc Ock", "Venom"]
+var sideKicks = ["Ironman", "Captain America", "Thor"]
 // Print the first sidekick to your console
-
+console.log(sideKicks[0])
 // Print the last archEnemy to the console
-
+console.log(archEnemies[2])
 // Write some code to add a new archEnemy to the archEnemies array
-
+archEnemies.push("Vulture")
 // Print the archEnemies array to console to ensure you added a new archEnemy
-
+console.log(archEnemies)
 // Remove the first sidekick from the sidekicks array
-
+sideKicks.shift()
 // Print the sidekicks array to console to ensure you added a new sidekick
-
+console.log(sideKicks)
 // Create a function called assessSituation that takes three arguments - dangerLevel, saveTheDay, badExcuse
 //   - dangerLevel should be an integer
-//   - saveTheDay should be a string a hero would say once they save the day 
+//   - saveTheDay should be a string a hero would say once they save the day
 //   - badExcuse should be a string a hero would say if they are too afraid of the dangerLevel
+function assessSituation(dangerLevel, saveTheDay, badExcuse) {
+  if (dangerLevel > 50) {
+    console.log(badExcuse)
+  } else if (dangerLevel <= 50 && dangerLevel >= 10) {
+    console.log(saveTheDay)
+  } else {
+    console.log("Meh. Hard pass.")
+  }
+}
 
+assessSituation(9, "I'm comin bruh", "Chill")
 // Your function should include an if/else statement that meets the following criteria
 //   - Danger levels that are above 50 are too scary for your hero. Any danger level that is above 50 should result in printing the badExcuse to the console
 //   - Anything dangerLevel that is between 10 and 50 should result in printing the saveTheDay string to the console
 //   - If the dangerLevel is below 10, it means it is not worth your time and should result in printing the string "Meh. Hard pass." to the console.
 
 //Test Cases
-var announcement = 'Never fear, the Courageous Curly Bracket is here!';
-var excuse = 'I think I forgot to lock up my 1992 Toyota Coralla. Be right back.';
+var announcement = "Never fear, the Courageous Curly Bracket is here!"
+var excuse =
+  "I think I forgot to lock up my 1992 Toyota Coralla. Be right back."
 // assessSituation(99, announcement, excuse) > Should print - 'I think I forgot to lock up my 1992 Toyota Coralla. Be right back.'
 //assessSituation(21, announcement, excuse) > should print - 'Never fear, the Courageous Curly Bracket is here!'
 //assessSituation(3, announcement, excuse) > should print - "Meh. Hard pass."
@@ -55,30 +73,62 @@ var excuse = 'I think I forgot to lock up my 1992 Toyota Coralla. Be right back.
 //   - citiesDestroyed (array)
 //   - luckyNumbers (array)
 //   - address (object with following key/values: number , street , state, zip)
-
-
+var scaryMonster = {
+  name: "greg",
+  smell: "delicious",
+  weight: 220,
+  citiesDestroyed: ["NYC", "LA", "Nola"],
+  luckyNumbers: [2, 7],
+  address: {
+    number: 678,
+    street: "Warren",
+    state: "NY",
+    zip: 11217,
+  },
+}
 // Create a new class called SuperHero
 // - Your class should have the following DYNAMIC values
-//   - name 
+//   - name
 //   - superpower
-//   - age 
+//   - age
 // - Your class should have the following STATIC values
 //   - archNemesis, assigned to "The Syntax Error"
 //   - powerLevel = 100
-//   - energyLevel = 50 
-
+//   - energyLevel = 50
+class SuperHero {
+  constructor(name, superpower, age) {
+    this.name = name
+    this.superpower = superpower
+    this.age = age
+    this.archNemesis = "The Syntax Error"
+    this.powerLevel = 100
+    this.energyLevel = 50
+  }
+  sayName() {
+    console.log("I'm " + this.name)
+  }
+  maximizeEnergy() {
+    console.log((this.energyLevel = 1000))
+  }
+  gainPower(power) {
+    console.log((this.powerLevel += power))
+  }
+}
 // - Create the following class methods
 //   - sayName, should print the hero's name to the console
 //   - maximizeEnergy, should update the energyLevel to 1000
 //   - gainPower, should take an argument of a number and INCREASE the powerLevel by that number
-
+var spiderman = new SuperHero("Spiderman", "Spider abilities", 25)
+var batman = new SuperHero("Batman", "Vengeance", 32)
 // - Create 2 instances of your SuperHero class
+console.log(batman)
 
-
+batman.sayName()
+spiderman.maximizeEnergy()
+batman.gainPower(50)
 // Reflection
 // What parts were most difficult about this exerise?
 
 // What parts felt most comfortable to you?
 
 // What skills do you need to continue to practice before starting Mod 1?
-
